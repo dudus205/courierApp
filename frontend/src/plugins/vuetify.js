db9130feb553cc10816e63fe3805b09619e08bda
Przemysław Postrch <przemyslaw.postrach@hotmail.com>
2022-06-24 16:45:38 +0200

    Add parcel table

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -3,5 +3,12 @@ import Vuetify from 'vuetify/lib/framework';
 
 Vue.use(Vuetify);
 
+
+import Pl from 'vuetify/lib/locale/pl'
+
 export default new Vuetify({
+    lang: {
+        locales: {Pl},
+        current: 'Pl',
+    },
 });
--- frontend/src/views/AppParcels.vue
+++ frontend/src/views/AppParcels.vue
@@ -1,10 +1,49 @@
 <template>
-  <p>Parcels</p>
+  <v-layout column>
+    <v-row>
+      <v-col cols="12" class="justify-end">
+        <v-btn depressed color="primary" class="mr-4">Wyznacz trasę</v-btn>
+        <v-btn depressed color="success">Dodaj paczkę</v-btn>
+      </v-col>
+      <v-col cols="12">
+        <v-data-table
+            :headers="headers"
+            :items="data"
+        ></v-data-table>
+      </v-col>
+    </v-row>
+  </v-layout>
 </template>
 
 <script>
 export default {
-  name: "AppParcels"
+  name: "AppParcels",
+  data() {
+    return {
+      headers: [
+        {
+          text: 'ID',
+          align: 'start',
+          sortable: false,
+          value: 'id',
+        },
+        {text: 'Ulica', value: 'street'},
+        {text: 'Numer domu', value: 'houseNumber'},
+        {text: 'Kraj', value: 'country'},
+        {text: 'Miasto', value: 'city'},
+        {text: 'Imię', value: 'name'},
+        {text: 'Nazwisko', value: 'surname'},
+      ],
+      data: []
+    }
+  },
+  async mounted() {
+    const response = await this.$axios.get('parcel');
+
+    if (response.status === 200) {
+      this.data = response.data;
+    }
+  }
 }
 </script>
 
